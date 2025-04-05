6aa36f5ac164c7e74c502935e598cf887cde9c64
Daniel Kandalaft <kandddany1985@gmail.com>
2025-04-05 14:08:23 +0300

    Added ExtentManager to initialize and configure ExtentReports instance

---
+++ src/main/java/utils/ExtentManager.java
@@ -0,0 +1,24 @@
+package utils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentManager {
+
+    private static ExtentReports extent;
+
+    public static ExtentReports getInstance() {
+        if (extent == null) {
+            String reportPath = System.getProperty("user.dir") + "/test-output/ExtentReport.html";
+            ExtentSparkReporter sparkReporter = new ExtentSparkReporter(reportPath);
+            sparkReporter.config().setReportName("Reqres API Test Report");
+            sparkReporter.config().setDocumentTitle("Automation Results");
+
+            extent = new ExtentReports();
+            extent.attachReporter(sparkReporter);
+            extent.setSystemInfo("Framework", "Rest Assured");
+            extent.setSystemInfo("Tester", "Your Name"); // You can personalize this
+        }
+        return extent;
+    }
+}
