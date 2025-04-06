5dfa3ce462df772479c84173c2f778061a8170d2
Daniel Kandalaft <kandddany1985@gmail.com>
2025-04-06 18:53:05 +0300

    Added ExtentTestListener to connect TestNG lifecycle to ExtentReports

---
+++ src/main/java/utils/ExtentTestListener.java
@@ -0,0 +1,41 @@
+package utils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ExtentTestListener implements ITestListener {
+
+    private static ExtentReports extent = ExtentManager.getInstance();
+    private static ThreadLocal<ExtentTest> extentTest = new ThreadLocal<>();
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        ExtentTest test = extent.createTest(result.getMethod().getMethodName());
+        extentTest.set(test);
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        extentTest.get().log(Status.PASS, "Test Passed");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        extentTest.get().log(Status.FAIL, "Test Failed");
+        extentTest.get().fail(result.getThrowable()); // log the error
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        extentTest.get().log(Status.SKIP, "Test Skipped");
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        extent.flush(); // important: writes everything to the report
+    }
+}
